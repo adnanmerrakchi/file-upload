d98594773e42220b6c26c07663cb87affa31cc18
adnanmerrakchi <adnanmerrakchi@gmail.com>
2021-08-13 00:42:22 +0200

    add thymleaf html file.

--- src/main/java/com/example/uploadingfiles/storage/StorageService.java
+++ src/main/java/com/example/uploadingfiles/storage/StorageService.java
@@ -1,10 +1,25 @@
 package com.example.uploadingfiles.storage;
 
 import org.springframework.stereotype.Service;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
 
 @Service
-public class StorageService {
+public interface StorageService {
 
+    void init();
 
+    void store(MultipartFile file);
+
+    Stream<Path> loadAll();
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
 
 }
